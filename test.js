ef2ecb4228df872cf2e25a6db80b06db962a4fdd
Vincent Weevers <mail@vincentweevers.nl>
2021-10-01 19:41:01 +0200

    Bump standard and nyc

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-var test = require('tape')
-var level = require('.')
+'use strict'
+
+const test = require('tape')
+const level = require('.')
 
 require('level-packager/abstract/base-test')(test, level)
 require('level-packager/abstract/db-values-test')(test, level, true)
